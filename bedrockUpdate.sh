75eca4aadca2a014a7f20fe561e2fe5fe735849f
ParFlesh <parflesh@gmail.com>
2020-09-06 11:11:26 -0500

    Workflow setup
    * create workflow files

--- bedrockUpdate.sh
+++ bedrockUpdate.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-VERSION=$(curl -v --silent  https://www.minecraft.net/en-us/download/server/bedrock/ 2>&1 | grep 'https://minecraft.azureedge.net/bin-linux/bedrock-server-.*\.zip' | awk -F'bedrock-server-' '{print $2}' | awk -F'.zip' '{print $1}')
+VERSION=$(curl -v -L --silent  https://www.minecraft.net/en-us/download/server/bedrock/ 2>&1 | grep 'https://minecraft.azureedge.net/bin-linux/bedrock-server-.*\.zip' | awk -F'bedrock-server-' '{print $2}' | awk -F'.zip' '{print $1}')
 
 BRANCH=$(git branch -a | grep "${VERSION}")
 if [ $? -ne 0 ]
 then
 	echo ${VERSION}
-	git checkout master;git checkout -b $VERSION
+	git checkout master;git checkout -b $VERSION > /dev/null 2>&1
 
-	sed -i "s#VERSION=\"latest\"#VERSION=\"${VERSION}\"#" Dockerfile
+	sed -i "s#VERSION=\"latest\"#VERSION=\"${VERSION}\"#" Dockerfile > /dev/null 2>&1
 
-	git commit -a -m "$VERSION"
-
-	git push --set-upstream origin $VERSION
+	git commit -a -m "$VERSION" > /dev/null 2>&1
 fi
