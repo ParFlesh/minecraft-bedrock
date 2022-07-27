63c794411188da703a11991b603e2d95d9293c78
ParFlesh <parflesh@gmail.com>
2022-07-27 11:02:13 -0500

    Update bedrockUpdate.sh

--- bedrockUpdate.sh
+++ bedrockUpdate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=$(curl -v -L --silent  https://www.minecraft.net/en-us/download/server/bedrock/ 2>&1 | grep 'https://minecraft.azureedge.net/bin-linux/bedrock-server-.*\.zip' | awk -F'bedrock-server-' '{print $2}' | awk -F'.zip' '{print $1}')
+VERSION=$(curl -v -L --silent -A 'Version/7.68.0' https://www.minecraft.net/en-us/download/server/bedrock/ 2>&1 | grep 'https://minecraft.azureedge.net/bin-linux/bedrock-server-.*\.zip' | awk -F'bedrock-server-' '{print $2}' | awk -F'.zip' '{print $1}')
 
 BRANCH=$(git branch -a | grep "${VERSION}")
 if [ $? -ne 0 ]
